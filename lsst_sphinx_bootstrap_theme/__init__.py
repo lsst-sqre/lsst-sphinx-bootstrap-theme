10f4aa074fb3bcc0f93e2c56e84045f6ac15fdae
Jonathan Sick <jsick@lsst.org>
2016-08-08 12:13:29 -0700

    Add get_html_templates_path

--- lsst_sphinx_bootstrap_theme/__init__.py
+++ lsst_sphinx_bootstrap_theme/__init__.py
@@ -8,5 +8,24 @@ __version_full__ = __version__
 
 
 def get_html_theme_path():
-    """Return list of HTML theme paths."""
+    """Path (str) of the sphinx HTML theme.
+
+    In a project's ``conf.py``,
+
+    ::
+       html_theme_path = [lsst_sphinx_bootstrap_theme.get_html_theme_path()]
+    """
     return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def get_html_templates_path():
+    """Path of reST templates provided with this theme.
+
+    In a project's ``conf.py``,
+
+    ::
+       templates_path = ['_templates',
+                         lsst_sphinx_bootstrap_theme.get_html_templates_path()]
+    """
+    return os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        'templates'))
